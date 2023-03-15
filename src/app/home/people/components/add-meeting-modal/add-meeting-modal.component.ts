35441483f9f54327b2c37126bdddfb944ade54f5
Kamil <qkiz288@gmail.com>
2023-03-15 09:39:43 +0100

    Added Impact dropdown

--- src/app/home/people/components/add-meeting-modal/add-meeting-modal.component.ts
+++ src/app/home/people/components/add-meeting-modal/add-meeting-modal.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddMeetingModalComponent implements OnInit {
 
+  impactValues = ['Low', 'Medium', 'High'];
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/ng-material/ng-material.module.ts
+++ src/app/ng-material/ng-material.module.ts
@@ -4,13 +4,15 @@ import { MatButtonModule } from '@angular/material/button'
 import { TextFieldModule } from '@angular/cdk/text-field'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
 
 const modules = [
   MatDialogModule,
   MatButtonModule,
   TextFieldModule,
   MatFormFieldModule,
-  MatInputModule
+  MatInputModule,
+  MatSelectModule
 ]
 
 @NgModule({
