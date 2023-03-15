bbc2207924339f8802873f77c4318ad2192eea4f
Kamil <qkiz288@gmail.com>
2023-03-15 09:56:57 +0100

    Added Attrition Risk dropdown

--- src/app/home/people/components/add-meeting-modal/add-meeting-modal.component.ts
+++ src/app/home/people/components/add-meeting-modal/add-meeting-modal.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AddMeetingModalComponent implements OnInit {
 
   impactValues = ['Low', 'Medium', 'High'];
+  attritionRiskValues = ['No Risk', 'Low', 'Medium', 'High'];
 
   constructor() { }
 
