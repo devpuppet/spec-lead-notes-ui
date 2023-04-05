a981661a3345ed8f5c9de8d2f17e43a86c6a3d16
Kamil <qkiz288@gmail.com>
2023-04-05 10:02:00 +0200

    Person component pagination

--- src/app/home/people/components/person/person.component.ts
+++ src/app/home/people/components/person/person.component.ts
@@ -15,19 +15,65 @@ export class PersonComponent implements OnInit {
   @Input()
   unitId!: string;
 
+  pagination: Pagination = {
+    config: {
+      itemsOnPage: 3,
+      currentPageIndex: 0,
+    },
+    pages: []
+  }
+
   constructor(private modalService: ModalService) { }
 
   ngOnInit(): void {
+    this.initializePagination();
   }
 
   openAddMeetingModal() {
     this.modalService
-    .open(AddMeetingModalComponent, {
-      data: { id: this.personData.id, name: this.personData.name, unitId: this.unitId }
-    })
-    .subscribe({ 
-      afterClosed: data => console.log('Data from Modal', data)
-    });
+      .open(AddMeetingModalComponent, {
+        data: { id: this.personData.id, name: this.personData.name, unitId: this.unitId }
+      })
+      .subscribe({
+        afterClosed: data => console.log('Data from Modal', data)
+      });
   }
 
+  updatePageNumber(index: number) {
+    this.pagination.config.currentPageIndex = +index;
+  }
+
+  private initializePagination() {
+    let pageIndex = 0;
+    this.pagination.pages.push({ data: [] });
+    for (let i = 0; i < this.personData.meetings.length; i++) {
+      this.pagination.pages[pageIndex].data.push(this.personData.meetings[i]);
+      if (this.isEndOfPage(i, pageIndex) && this.hasMoreData(i, this.personData.meetings)) {
+        pageIndex++;
+        this.pagination.pages.push({ data: [] });
+      }
+    }
+  }
+
+  isEndOfPage(i: number, pageIndex: number) {
+    return i >= (this.pagination.config.itemsOnPage - 1) + pageIndex * this.pagination.config.itemsOnPage;
+  }
+
+  hasMoreData(i: number, data: any[]) {
+    return data.length > i + 1;
+  }
+}
+
+export interface Pagination {
+  config: PaginationConfig,
+  pages: Page[]
+}
+
+export interface PaginationConfig {
+  itemsOnPage: number,
+  currentPageIndex: number,
+}
+
+export interface Page {
+  data: any[]
 }
