32ee7b9325a1f66ca5eae12e9991a47317abfd3d
Kamil <qkiz288@gmail.com>
2023-05-16 09:18:25 +0200

    switchMap operator

--- src/app/home/people/components/edit-meeting/edit-meeting.component.ts
+++ src/app/home/people/components/edit-meeting/edit-meeting.component.ts
@@ -3,6 +3,10 @@ import { AttritionRisk, MeetingNotes } from '../../models/meeting-notes.model';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { YesNoToBooleanMapper } from 'src/app/shared/mappers/yes-no.mapper';
 import { Location } from '@angular/common';
+import { debounceTime, switchMap } from 'rxjs';
+import { UnitsService } from '../../services/units.service';
+import { Store } from '@ngrx/store';
+import { selectUserId } from 'src/app/login/store/login.selectors';
 
 @Component({
   selector: 'app-edit-meeting',
@@ -12,6 +16,7 @@ import { Location } from '@angular/common';
 export class EditMeetingComponent implements OnInit {
 
   meetingData!: MeetingNotes;
+  notesId?: string;
   meetingForm!: FormGroup;
   cdkAutosizeMinRows = 1;
   cdkAutosizeMaxRows = 4;
@@ -25,10 +30,13 @@ export class EditMeetingComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private yesNoMapper: YesNoToBooleanMapper,
-    private location: Location) { }
+    private location: Location,
+    private store: Store,
+    private unitsService: UnitsService) { }
 
   ngOnInit(): void {
     this.meetingData = history.state;
+    this.notesId = this.meetingData.notesId;
     this.meetingForm = this.formBuilder.group({
       comments: [this.meetingData.comments],
       questions: [this.meetingData.questions],
@@ -42,6 +50,42 @@ export class EditMeetingComponent implements OnInit {
       attritionRisk: [this.meetingData.attritionRisk],
       oneToOneReportSent: [this.yesNoMapper.toModel(this.meetingData.oneToOneReportSent)]
     });
+
+    let userId: string | undefined;
+    this.store.select(selectUserId)
+      .subscribe(value => userId = value);
+
+    if (!userId) {
+      throw new Error('userId not found in Store');
+    }
+
+    // Below subscription to valueChanges event was implemented to exercise switchMap operator
+    // Behavior in UI: when user edits Comments form field, request is sent to fetch data from BE 
+    // and update Questions form field if no other Observable is emitted from valueChanges for 2 seconds
+    this.comments.valueChanges
+      .pipe(
+        debounceTime(2000),
+        switchMap(() => this.unitsService.getUnits(userId!))
+      )
+      .subscribe(units => {
+        const meetingData = units
+          .flatMap(unit => unit.people)
+          .flatMap(person => person.meetings)
+          .find(meeting => meeting.notesId === this.notesId)!;
+
+        setTimeout(() => {
+          this.questions.setValue(meetingData.questions);
+        })
+      }
+      );
+  }
+
+  get comments() {
+    return this.meetingForm.get('comments')!;
+  }
+
+  get questions() {
+    return this.meetingForm.get('questions')!;
   }
 
   onSubmit() {
