992c9a489ad4b2e6e997a788770f51040cde4150
Kamil <qkiz288@gmail.com>
2023-05-17 16:27:03 +0200

    Components with OnPush change detetction strategy

--- src/app/home/people/components/unit/unit.component.ts
+++ src/app/home/people/components/unit/unit.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { Unit } from 'src/app/home/people/models/unit.model';
 
 @Component({
   selector: 'app-unit',
   templateUrl: './unit.component.html',
-  styleUrls: ['./unit.component.css']
+  styleUrls: ['./unit.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UnitComponent implements OnInit {
 
